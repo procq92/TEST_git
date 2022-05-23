3d934fde588cc17d18ade6d2580b85c6a7382772
Pascal Rocq <pascal.rocq+git@gmail.com>
2022-05-23 08:54:39 +0200

    Initial Commit

---
+++ TEST_git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TEST_git
+//
+//  Created by Pascal Rocq on 23/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TEST_git/TEST_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEST_gitApp.swift
+//  TEST_git
+//
+//  Created by Pascal Rocq on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TEST_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
